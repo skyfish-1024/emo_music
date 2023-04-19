912decd93f5b6597a31be2149d22ea49298cba8a
zzyzzyzzyzz <1982452293@qq.com>
2023-04-19 21:50:36 +0800

    emo_music

---
+++ src/components/Triangle.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="triangle">
+    <div class="triangle2" v-if="isOn"></div>
+    <div class="triangle1" v-else></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isOn: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+<style lang="scss">
+.triangle {
+  margin-left: 0.05rem;
+}
+.triangle1 {
+  height: 0;
+  width: 0;
+  border: 0.08rem solid;
+  transform: translateY(0.05rem) translateX(0.05rem) rotateZ(90deg);
+  border-color: transparent transparent transparent #cccccc;
+}
+.triangle2 {
+  height: 0;
+  width: 0;
+  border: 0.08rem solid;
+  transform: translateX(0.05rem) rotateZ(0deg);
+  border-color: transparent transparent transparent #cccccc;
+}
+</style>
